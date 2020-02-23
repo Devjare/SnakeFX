b3f07da6834002320c91e509629f76bee3e8e8fc
Andres Reyna <andresreyna15@gmail.com>
2020-02-22 21:01:20 -0600

    Added drawing for snake & timer

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,105 @@
 
+import javax.swing.JComboBox.KeySelectionManager;
+
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
+import java.util.List;
+
 public class Main extends Application {
-	
+
 	@Override
 	public void start(Stage primaryStage) {
-		
+
 		final int WIDTH = 500;
 		final int HEIGHT = 500;
-		
+
 		var root = new BorderPane();
 		var scene = new Scene(root, WIDTH, HEIGHT);
-		
+
 		var snake = new Snake(new Snake.Corner(WIDTH / 2, HEIGHT / 2));
 		System.out.println(snake);
-		
+
 		scene.addEventFilter(KeyEvent.KEY_PRESSED, key -> {
-			
+
+			var code = key.getCode();
+			var snakeDirection = snake.getDirection();
+
+			if (code.equals(KeyCode.RIGHT)
+					&& !(snakeDirection.equals(Snake.Direction.LEFT) || snakeDirection.equals(Snake.Direction.RIGHT)))
+				snake.moveRight();
+			else if (code.equals(KeyCode.DOWN)
+					&& !(snakeDirection.equals(Snake.Direction.UP) || snakeDirection.equals(Snake.Direction.DOWN)))
+				snake.moveDown();
+			else if (code.equals(KeyCode.LEFT)
+					&& !(snakeDirection.equals(Snake.Direction.LEFT) || snakeDirection.equals(Snake.Direction.RIGHT)))
+				snake.moveLeft();
+			else if (code.equals(KeyCode.UP)
+					&& !(snakeDirection.equals(Snake.Direction.UP) || snakeDirection.equals(Snake.Direction.DOWN)))
+				snake.moveUp();
+
 		});
-		
+
+		var timer = new AnimationTimer() {
+
+			long lastTick = 0;
+
+			@Override
+			public void handle(long now) {
+
+				clearScreen();
+				drawSnake();
+				
+				long tick = now / 1000000000L;
+
+				if (lastTick != tick) {
+
+					System.out.println("Snake body: " + snake.getCornersPositions());
+					
+					var snakeDirection = snake.getDirection();
+
+					if (snakeDirection.equals(Snake.Direction.RIGHT)) {
+						snake.moveRight();
+					} else if (snakeDirection.equals(Snake.Direction.DOWN)) {
+						snake.moveDown();
+					} else if (snakeDirection.equals(Snake.Direction.LEFT)) {
+						snake.moveLeft();
+					} else {
+						snake.moveUp();
+					}
+
+					lastTick = tick;
+				}
+
+			}
+
+			private void clearScreen() {
+				root.getChildren().clear();
+			}
+
+			private void drawSnake() {
+				var corners = snake.getCorners();
+				corners.forEach(corner -> {
+					var circle = new Circle();
+					circle.setCenterX(corner.x);
+					circle.setCenterY(corner.y);
+					circle.setRadius(5);
+					circle.setFill(Color.BLACK);
+					
+					root.getChildren().add(circle);
+				});
+			}
+		};
+
+		timer.start();
+
 		primaryStage.setTitle("Snake!");
 		primaryStage.setScene(scene);
 		primaryStage.show();
--- src/Snake.java
+++ src/Snake.java
@@ -10,43 +10,121 @@ public class Snake {
 			this.x = x;
 			this.y = y;
 		}
+
+		@Override
+		public String toString() {
+			return "[x: " + x + ", y: " + y + "]";
+		}
 	}
-	
+
 	static enum Direction {
 		UP, DOWN, LEFT, RIGHT
 	}
-	
-	private List<Corner> corners;
+
+	private ArrayList<Corner> corners;
 	private int length;
 	private Direction direction;
+	private int speed = 5;
 
 	public Snake(Corner headCorner) {
-		this.corners = List.of(headCorner, new Corner(headCorner.x - 5, headCorner.y),
-				new Corner(headCorner.x - 10, headCorner.y));
+		this.corners = new ArrayList<Corner>();
+		this.corners.add(headCorner);
+		this.corners.add(new Corner(headCorner.x - 5, headCorner.y));
+		this.corners.add(new Corner(headCorner.x - 10, headCorner.y));
+
 		this.direction = Direction.RIGHT;
 		this.length = corners.size();
 	}
 
 	public void moveRight() {
+		
 		this.direction = Direction.RIGHT;
+
+		var head = this.corners.get(0);
+
+		for (int i = 0; i < this.corners.size() - 1; i++) {
+			if (i > 1) {
+				
+				this.corners.set(i, this.corners.get(i - 1));
+			} else if (i == 0) {
+				this.corners.get(i).x += speed;
+			} else {
+				if(this.direction.equals(Direction.RIGHT))
+					this.corners.set(1, head);
+			}
+		}
 	}
-	
+
 	public void moveLeft() {
+
 		this.direction = Direction.LEFT;
+
+		var head = this.corners.get(0);
+
+		for (int i = 0; i < this.corners.size() - 1; i++) {
+			if (i > 1) {
+				this.corners.set(i, this.corners.get(i - 1));
+			} else if (i == 0) {
+				this.corners.get(i).x -= speed;
+			} else {
+				this.corners.set(i, head);
+			}
+		}
 	}
-	
+
 	public void moveUp() {
+
 		this.direction = Direction.UP;
+		
+		var head = this.corners.get(0);
+
+		for (int i = 0; i < this.corners.size() - 1; i++) {
+			if (i > 1) {
+				this.corners.set(i, this.corners.get(i - 1));
+			} else if (i == 0) {
+				this.corners.get(i).y -= speed;
+			} else {
+				this.corners.set(i, head);
+			}
+		}
 	}
-	
+
 	public void moveDown() {
+		
 		this.direction = Direction.DOWN;
+		
+		var head = this.corners.get(0);
+
+		for (int i = 0; i < this.corners.size() - 1; i++) {
+			if (i > 1) {
+				this.corners.set(i, this.corners.get(i - 1));
+			} else if (i == 0) {
+				this.corners.get(i).y += speed;
+			} else {
+				this.corners.set(i, head);
+			}
+		}
+	}
+
+	public Direction getDirection() {
+		return this.direction;
+	}
+
+	public Corner getHeadCorner() {
+		return this.corners.get(0);
+	}
+
+	public ArrayList<Corner> getCorners() {
+		return this.corners;	
 	}
 	
 	@Override
 	public String toString() {
 		return "Snake [corners=" + corners + ", length=" + length + ", direction=" + direction + "]";
 	}
-	
-	
+
+	public String getCornersPositions() {
+		return corners.toString();
+	}
+
 }
